f7d455ee926c20ef4bef7077025bc8695b34d2b5
Ivan Miljancic <ivan.miljancic@gmail.com>
2019-06-30 19:08:33 +0200

    Design finished

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="question-box">
+    <b-jumbotron>
+      <template slot="lead">
+        Some question?
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next question</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionBox"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+    <b-nav tabs>
+      <b-nav-item disabled><strong>Quizz App</strong></b-nav-item>
+      <b-nav-item disabled><strong>Correct answers: 4/10 </strong></b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+};
+</script>
+
+<style>
+
+</style>
